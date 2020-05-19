3c485fd74247c62164388d1a593604c675f47b40
adler zamora <adlerzamora@gmail.com>
2020-05-18 21:42:06 -0500

    Created Functions for Value and Percentage Dif

--- webDriver.py
+++ webDriver.py
@@ -1,22 +1,66 @@
 from selenium import webdriver
-# Using Chrome to access web
-driver = webdriver.Chrome('D:\ETORO\Prerequesites\chromedriver')
-# Open the website
-driver.get('https://www.etoro.com/login')
+from selenium.webdriver.common.keys import Keys
+import time
 
 
-# Select the id box - USERNAME
-id_box = driver.find_element_by_name('username')
-# Send id information
-id_box.send_keys('$$$$$')
+def initializeChromWebDriver():
+    # Using Chrome to access web - Driver URL depending on your saved file
+    driver = webdriver.Chrome('D:\\ETORO\\Prerequesites\\chromedriver')
+    return driver
 
 
-# Find password box
-pass_box = driver.find_element_by_name('password')
-# Send password
-pass_box.send_keys('$$$$')
+def logIn(driver):
+    # Open the website
+    driver.get('https://www.etoro.com/login')
 
-# Find login button
-login_button = driver.find_element_by_tag_name('button')
-# Click login
-login_button.click()
+    # Selects the id box - USERNAME
+    id_box = driver.find_element_by_name('username')
+
+    # Sends id information
+    id_box.send_keys('$$$$$')
+
+    time.sleep(5)
+
+    # Finds password box
+    pass_box = driver.find_element_by_name('password')
+    # Sends password
+    pass_box.send_keys('$$$$')
+
+    time.sleep(5)
+    # Finds login button
+    login_button = driver.find_element_by_tag_name('button')
+    # Click login
+    login_button.click()
+    time.sleep(5)
+
+
+def goToStats(driver):
+    # Goes to Interested Stock Stats
+    driver.get('https://www.etoro.com/markets/tsla/stats')
+
+
+def returnValue(driver):
+
+    tslCurrentValue = driver.find_element_by_css_selector(
+        'span.head-info-stats-value')
+
+    print("Tesla Current Value: " + str(tslCurrentValue.text))
+
+
+def returnPercentage(driver):
+    # Gets todays percentage and value difference from openning
+    tslTodayPercentage = driver.find_element_by_css_selector(
+        'span.head-info-stats-change')
+
+    # Separates WebElement by Quantity & Percentage
+    tdyPtgChunks = tslTodayPercentage.text.split(' ')
+
+    # Prints separated values ready to use for further learning functions
+    print("Tesla's Today Price Difference: " + str(tdyPtgChunks[0]))
+    print("Tesla's Today Percentage Difference: " + str(tdyPtgChunks[1]))
+
+
+driver = initializeChromWebDriver()
+goToStats(driver)
+returnValue(driver)
+returnPercentage(driver)
